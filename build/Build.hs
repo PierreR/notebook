0b395f46ff82c2896399e1897f73ccef3d03fbb9
Pierre Radermecker <pierrer@pi3r.be>
2016-08-02 00:49:03 +0200

    Add comment

--- build/Build.hs
+++ build/Build.hs
@@ -7,6 +7,8 @@ import Development.Shake.Util
 buildDir = "out"
 srcDir = "src"
 
+-- | Add the extension as a directory to better organize output
+-- "out/test.html" -> "out/html/test.html"
 addExtensionDir :: FilePath -> FilePath
 addExtensionDir fp  = takeDirectory fp </> (drop 1 . takeExtension) fp </> takeFileName fp
 
@@ -16,11 +18,14 @@ dropExtensionDir fp =
   in
   (takeDirectory . dropTrailingPathSeparator) fp' </> fn
 
-
-callCmd execmd out = do
-    let  doc = srcDir </> (dropDirectory1 . dropExtensionDir)  (out -<.> "adoc")
-    need [doc]
-    cmd execmd doc "-o" out
+-- | Exec the asciidoc command to produce html or pdf
+callCmd prg out = do
+  -- find back the adoc source file by dropping the `out` prefix and the extension directory 
+  let  srcfile = srcDir </> (dropDirectory1 . dropExtensionDir)  (out -<.> "adoc")
+  -- require the source file so the rule is only fired when the source changes
+  need [srcfile]
+  -- call asciidoctor with the -o flag to control the output generated file path
+  cmd prg srcfile "-o" out
 
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="build/_shake"} $ do
@@ -36,8 +41,10 @@ main = shakeArgs shakeOptions{shakeFiles="build/_shake"} $ do
 
   "html" ~> do
     is <- getDirectoryFiles srcDir ["/**/*.adoc"]
+    -- require the exact wanted output
     need [ buildDir </> addExtensionDir (i -<.> "html") | i <- is ]
 
   "pdf" ~> do
     is <- getDirectoryFiles srcDir ["/**/*.adoc"]
-    need [ buildDir </> addExtensionDir (i-<.> "pdf") | i <- is ]
+    -- require the exact wanted output
+    need [ buildDir </> addExtensionDir (i -<.> "pdf") | i <- is ]
