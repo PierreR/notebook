2741c7bd00732117568dfafe45a2d2ac07dfd6f4
Pierre Radermecker <pierrer@pi3r.be>
2016-10-29 17:59:55 +0200

    Use a central notebook

--- build/Build.hs
+++ build/Build.hs
@@ -7,44 +7,31 @@ import Development.Shake.Util
 buildDir = "out"
 srcDir = "src"
 
--- | Add the extension as a directory to better organize output
--- "out/test.html" -> "out/html/test.html"
-addExtensionDir :: FilePath -> FilePath
-addExtensionDir fp  = takeDirectory fp </> (drop 1 . takeExtension) fp </> takeFileName fp
-
-dropExtensionDir :: FilePath -> FilePath
-dropExtensionDir fp =
-  let (fp', fn) = splitFileName fp
-  in
-  (takeDirectory . dropTrailingPathSeparator) fp' </> fn
-
 -- | Exec the asciidoc command to produce html or pdf
 callCmd prg out = do
-  -- find back the adoc source file by dropping the `out` prefix and the extension directory 
-  let  srcfile = srcDir </> (dropDirectory1 . dropExtensionDir)  (out -<.> "adoc")
+  -- find back the adoc source file by dropping the `out` prefix and the extension directory
+  let  srcfile = srcDir </> dropDirectory1  (out -<.> "adoc")
   -- require the source file so the rule is only fired when the source changes
   need [srcfile]
+  need =<< getDirectoryFiles "" ["src/**/*.adoc"]
+  need [ srcDir </> "docinfo.html"]
   -- call asciidoctor with the -o flag to control the output generated file path
   cmd prg srcfile "-o" out
 
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="build/_shake"} $ do
 
-  buildDir <> "/**/*.html" %> callCmd "asciidoctor"
+  want [buildDir </> "notebook.html", buildDir </> "notebook.pdf"]
 
-  buildDir <> "/**/*.pdf" %> callCmd "asciidoctor-pdf"
+  buildDir </> "notebook.html" %> callCmd "asciidoctor"
+
+  buildDir </> "notebook.pdf" %> callCmd "asciidoctor-pdf"
 
   "clean" ~> do
     putNormal ("Cleaning files in " <> buildDir)
-    removeFilesAfter buildDir ["/**/*.html"]
-    removeFilesAfter buildDir ["/**/*.pdf"]
+    removeFilesAfter buildDir ["/*.html"]
+    removeFilesAfter buildDir ["/*.pdf"]
 
-  "html" ~> do
-    is <- getDirectoryFiles srcDir ["/**/*.adoc"]
-    -- require the exact wanted output
-    need [ buildDir </> addExtensionDir (i -<.> "html") | i <- is ]
+  "html" ~> need [ buildDir </> "notebook.html"]
 
-  "pdf" ~> do
-    is <- getDirectoryFiles srcDir ["/**/*.adoc"]
-    -- require the exact wanted output
-    need [ buildDir </> addExtensionDir (i -<.> "pdf") | i <- is ]
+  "pdf" ~> need [ buildDir </> "notebook.pdf"]
--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash
+#! nix-shell -i bash -p '(import <nixpkgs> {}).haskellPackages.ghcWithPackages (p: with p; [shake])'
 mkdir -p build/_shake
 ghc --make build/Build.hs -rtsopts -with-rtsopts=-I0 -outputdir=build/_shake -o build/_shake/build && build/_shake/build "$@"
-
