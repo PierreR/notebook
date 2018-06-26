8e2492c605321c70dfbc321399df7b212d3e66d0
Pierre Radermecker <pradermecker@cirb.brussels>
2018-06-26 16:02:21 +0200

    Replace runCommand with mkShell

--- shell.nix
+++ shell.nix
@@ -3,6 +3,8 @@
 with pkgs;
 let
 ghc = haskellPackages.ghcWithPackages (p: with p; [shake]);
-scriptfile = ./build/Build.hs;
 in
-runCommand "asciidoc-runner" { buildInputs = [ asciidoctor pythonPackages.pygments ghc ]; } "echo use nix-shell"
+mkShell {
+  name = "asciidoc-env";
+  buildInputs = [ asciidoctor pythonPackages.pygments ghc ];
+}
