d2262f4d81e540de40a1cbf09f0ca1850ce01378
Pierre Radermecker <pradermecker@cirb.brussels>
2019-10-30 15:40:17 +0100

    Remove deprecated folder

--- build/watch.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p entr
-cmd='./build/build.sh html'
-#cmd='build/build.sh html && build/build.sh pdf'
-while true; do ls *.adoc | entr sh -c "$cmd"; done
